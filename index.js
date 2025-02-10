30374132e836b72e3e537e9cad56a58b7bef15a0
Bobby Roe <bobby.roe@unifyconsulting.com>
2025-02-10 14:47:01 -0800

    rainbow stuff

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import { positionLocal } from "three/tsl";
+import { varying, tangentWorld, modelNormalMatrix, normalLocal } from "three/tsl";
 import getLayer from "./libs/getLayer.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 const w = window.innerWidth;
@@ -15,24 +15,14 @@ document.body.appendChild(renderer.domElement);
 const ctrls = new OrbitControls(camera, renderer.domElement);
 ctrls.enableDamping = true;
 
-const geometry = new THREE.TorusKnotGeometry(1.5, 0.5, 128, 32);
-
-// const vsh = await fetch('./src/vert.glsl');
-// const fsh = await fetch('./src/frag.glsl');
-
-// const uniforms = {
-//   time: { value: 0.0 },
-//   resolution: { value: new THREE.Vector2( w, h )},
-// };
+const geometry = new THREE.TorusKnotGeometry(1.5, 0.5, 256, 64);
 
 const material = new THREE.MeshStandardNodeMaterial({
-  // uniforms,
-  // vertexShader: await vsh.text(),
-  // fragmentShader: await fsh.text()
 });
-material.colorNode = positionLocal;
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const normalView = varying( modelNormalMatrix.mul( tangentWorld ) );
+material.colorNode = normalView.normalize();
+const knot = new THREE.Mesh(geometry, material);
+scene.add(knot);
 
 const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
 scene.add(hemiLight);
@@ -51,9 +41,7 @@ scene.add(sprites);
 function animate(t = 0) {
   t *= 0.001;
   requestAnimationFrame(animate);
-
-  // uniforms.resolution.value.set(renderer.domElement.width, renderer.domElement.height);
-  // uniforms.time.value = t;
+  knot.rotation.x = t;
   renderer.renderAsync(scene, camera);
   ctrls.update();
 }
